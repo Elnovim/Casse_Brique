c9ffa2a8010b1228d9984ca141d2e4272a85fa2c
tvoillemin <16646063+tvoillemin@users.noreply.github.com>
2019-08-28 13:50:45 +0200

    Update unix_platform.c

--- Unix/Code/unix_platform.c
+++ Unix/Code/unix_platform.c
@@ -163,8 +163,8 @@ int main (int argc, char *argv[]) {
 		if (input.buttons[BUTTON_F5].is_down && input.buttons[BUTTON_F5].changed) {
 
 			display_size_ind = (display_size_ind + 1) % DISPLAY_SIZE_COUNT;
-			int width = min(max_w, display_sizes[display_size_ind][0]);
-			int height = min(max_h, display_sizes[display_size_ind][1]);
+			width = min(max_w, display_sizes[display_size_ind][0]);
+			height = min(max_h, display_sizes[display_size_ind][1]);
 			if (width >= max_w || height >= max_h) display_size_ind = DISPLAY_SIZE_COUNT - 1;
 
 			render_buffer.width = width;
